e90fd16b8934eb4f9d6be8ad4aa04cadf2a5656c
imrushikesh77 <rushikeshjawale2004@gmail.com>
2024-03-19 11:42:53 +0530

    redis configured

--- backend/redis/redis.js
+++ backend/redis/redis.js
@@ -1,13 +1,13 @@
+
 import { createClient } from 'redis';
 
-const client = createClient();
+const client = createClient({
+    url: process.env.REDIS_URL
+});
+(async () => {
+  client.on('error', (err) => console.log('Redis Client Error', err));
 
-const connectRedis = async () => {
-    client.on('error', err => console.log('Redis Client Error', err));
-
-    await client.connect().then(() => console.log('Redis Connected'));
-};
-
-connectRedis();
+  await client.connect();
+})();
 
 export default client;
